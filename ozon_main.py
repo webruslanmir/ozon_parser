7b1fc159a12a0e19e4200c5ac4de6c18af9383a0
webruslanmir <webruslanmir@gmail.com>
2023-11-17 01:46:37 +0300

    Upd

--- ozon_main.py
+++ ozon_main.py
@@ -8,7 +8,7 @@ class OzonSellerParse:
 
     def __page_down(self, page):
         page.evaluate('''
-                                const scrollStep = 200; // Размер шага прокрутки (в пикселях)
+                                const scrollStep = 2000; // Размер шага прокрутки (в пикселях)
                                 const scrollInterval = 100; // Интервал между шагами (в миллисекундах)
 
                                 const scrollHeight = document.documentElement.scrollHeight;
@@ -26,6 +26,7 @@ class OzonSellerParse:
     def __get_seller_name(self, url: str):
         self.page2 = self.context.new_page()
         self.page2.goto(url=url)
+        # time.sleep(5)
 
         self.__page_down(self.page2)
         try:
@@ -34,7 +35,10 @@ class OzonSellerParse:
             seller_name = elements[1].inner_text()
             print(seller_name)
         except Exception:
+            print('Seller not found')
             return
+        # finally:
+        #     self.page2.close()
 
     def __get_links(self):
         self.page.wait_for_selector("#paginatorContent")
@@ -45,21 +49,25 @@ class OzonSellerParse:
         links = search_result.query_selector_all(".tile-hover-target")
         print(len(links))
         for count, link in enumerate(links):
-            if count > 5: break
-            url = "https://ozon.by" + link.get_attribute('href')
+            if count > 10: break
+            url = "https://ozon.ru" + link.get_attribute('href')
             self.__get_seller_name(url=url)
 
     def parse(self):
         with sync_playwright() as playwright:
-            browser = playwright.chromium.launch(headless=False)
-            self.context = browser.new_context()
+            browser = playwright.chromium.launch(headless=False, args=["--disable-blink-features=AutomationControlled"])
+            self.context = browser.new_context(geolocation={'latitude': 55.751244, 'longitude': 37.618423},
+                                               permissions=["notifications"])
             self.page = self.context.new_page()
-            self.page.goto("https://ozon.by/")
-            self.page.get_by_placeholder("Искать на Ozon").type(text=self.keyword, delay=0.3)
-            self.page.query_selector("button[type='submit']").click()
+            self.page.goto("https://ozon.ru/")
+            # self.page.goto("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")
+            time.sleep(5)
+            self.page.get_by_placeholder("Искать на Ozon").type(text=self.keyword, delay=0.5)
+            self.page.query_selector("button[aria-label='Поиск']").click()
             self.__get_links()
 
 
 if __name__ == "__main__":
-    OzonSellerParse("вентилятор").parse()
+    oz = OzonSellerParse("люстра")
+    oz.parse()
     time.sleep(20)
