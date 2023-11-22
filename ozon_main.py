8b83c38a53abcce0b6af64b3ca05c406f6c381c8
webruslanmir <webruslanmir@gmail.com>
2023-11-22 18:36:47 +0300

    Работает но утечка процессора

--- ozon_main.py
+++ ozon_main.py
@@ -60,16 +60,6 @@ class OzonSellerParse:
 
     def __get_seller_list(self):
         seller_list = []
-        seller_info = self.page.query_selector_all('a[href^="/seller?category="]')
-        for el in seller_info:
-            end_of_link = el.get_attribute('href')
-            soup = BeautifulSoup(el.inner_html(), 'html.parser')
-            title, count = soup.findAll('div')[0].findAll('span')
-            seller_list.append([title.text.strip(), count.text.strip(), f'{head_of_link}{end_of_link}'])
-        return seller_list
-
-    def parse(self):
-        sellers_with_categories = {}
         with sync_playwright() as playwright:
             browser = playwright.chromium.launch(
                 headless=False,
@@ -84,14 +74,33 @@ class OzonSellerParse:
             self.page.goto("https://www.ozon.ru/seller")
             # self.page.goto("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")
             time.sleep(.5)
-            seller_list = self.__get_seller_list()
+            seller_info = self.page.query_selector_all('a[href^="/seller?category="]')
+            for el in seller_info:
+                end_of_link = el.get_attribute('href')
+                soup = BeautifulSoup(el.inner_html(), 'html.parser')
+                title, count = soup.findAll('div')[0].findAll('span')
+                seller_list.append([title.text.strip(), count.text.strip(), f'{head_of_link}{end_of_link}'])
+            return seller_list
 
-            for seller in seller_list:
+    def parse(self):
+        sellers_with_categories = {}
+        seller_list = self.__get_seller_list()
+        for seller in seller_list:
+            with sync_playwright() as playwright:
+                browser = playwright.chromium.launch(
+                    headless=False,
+                    args=[
+                        "--disable-blink-features=AutomationControlled",
+                    ],
+
+                )
+                self.context = browser.new_context(geolocation={'latitude': 55.751244, 'longitude': 37.618423},
+                                                   permissions=["notifications"])
+                self.page = self.context.new_page()
 
                 sellers_with_categories[seller[0]] = {'count': seller[1], }
                 self.page.goto(f"{seller[2]}")
                 count_first = 0
-                # count_last = 0
                 repeat_count = 0
                 while repeat_count < 30:
 
@@ -147,7 +156,7 @@ class OzonSellerParse:
                     # self.page.click('button:has-text("О магазине")')
                     # try:
                     #     self.page.wait_for_selector('span.tsHeadline600Medium')
-                    # except:
+                    # except:chi
                     #     print('Except_block')
                     #     self.page.goto(f"{shop_link}")
                     #     self.page.click('button:has-text("О магазине")')
